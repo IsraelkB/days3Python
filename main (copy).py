16a42b0b78298abae12213365422719400e185b8
IsraelkB <136883574+IsraelkB@users.noreply.github.com>
2023-07-25 23:45:34 +0300

    Add files via upload

---
+++ main (copy).py
@@ -0,0 +1,46 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+side = input('your at a cross road. where do you want to go? type "left" or "right"\n')
+if side == "left":
+  position = input('you come to a lake. there is a islend in the middle of the lake. type "wait" for a boat. type "swim" to swim across.\n')
+  if position != "wait":
+    print("you Attacked by trout. Game Over.\n")
+  elif position == "wait":
+    doors = input('you arrive at the islend unharmed. there is a house with 3 doors. one red, one yellow and one blue. which colour do you choose?\n')
+    if doors == "blue":
+      print("you Eaten by beasts. Game Over.\n")
+    elif doors == "red":
+      print("you Burned by fire. Game Over.\n")
+    elif doors == "yellow":
+      print("You Win!")
+    else:
+      print("game over.")
+else:
+  print("you Fall into a hole. Game Over.")
+    
